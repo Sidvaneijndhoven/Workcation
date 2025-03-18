be07a88e4e901cdf2d2d018746d9b9b9f1dd203e
Sid <Sid.vaneijndhoven@edu-kw1c.nl>
2025-03-18 14:12:54 +0100

    Added footers and contact

--- src/App.vue
+++ src/App.vue
@@ -99,29 +99,21 @@
             </div>
           </div>
         </section>
-
-      <!-- Footer -->
-      <footer class="bg-gray-800 text-white py-6">
-        <div class="max-w-screen-xl mx-auto text-center">
-          <p>&copy; 2025 HouseMarket. All Rights Reserved.</p>
-          <div class="space-x-6 mt-4">
-            <a href="#" class="text-gray-400 hover:text-white">Facebook</a>
-            <a href="#" class="text-gray-400 hover:text-white">Twitter</a>
-            <a href="#" class="text-gray-400 hover:text-white">Instagram</a>
-          </div>
-        </div>
-      </footer>
+        <!-- Footer -->
+         <Footer></Footer>
     </div>  
   </div>
 </template>
 
 <script>
 import Nav from './components/Nav.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    Nav
+    Nav,
+    Footer
   }
 }
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+      <!-- Footer -->
+      <footer class="bg-gray-800 text-white py-6">
+        <div class="max-w-screen-xl mx-auto text-center">
+          <p>&copy; 2025 HouseMarket. All Rights Reserved.</p>
+          <div class="space-x-6 mt-4">
+            <a href="#" class="text-gray-400 hover:text-white">Facebook</a>
+            <a href="#" class="text-gray-400 hover:text-white">Twitter</a>
+            <a href="#" class="text-gray-400 hover:text-white">Instagram</a>
+          </div>
+        </div>
+      </footer>
+</template>
+
+<style scoped>
+    Footer 
+    {
+        margin-top: 100px;
+    }
+
+</style>
+  
